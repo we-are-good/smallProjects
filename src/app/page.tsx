ca7f7b1b9ddcc81db2d1dc26006395c7b5c5259e
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-06-03 14:35:00 +0900

    [feat] 메인 페이지 제목 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col gap-y-10 m-10">
+      <article className="font-bold">
+        항목을 클릭하면 적용된 페이지가 보입니다.
+      </article>
       <article className="flex flex-row gap-x-10">
         <Link href={"/api/schoolAPI"}>학교 API 적용</Link>
         <Link href={"/api/mapAPI"}>지도 API 적용</Link>
