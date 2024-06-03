49ebc0ea6e9c52646161edf37e177f7f169e7622
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-06-03 14:30:44 +0900

    [feat] 페이지 구성 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col gap-y-10 m-10">
+      <article className="flex flex-row gap-x-10">
+        <Link href={"/api/schoolAPI"}>학교 API 적용</Link>
+        <Link href={"/api/mapAPI"}>지도 API 적용</Link>
+      </article>
+      <article className="flex flex-row gap-x-10">
+        <Link href={"/pages/pagenation"}>Pagenation</Link>
+        <Link href={"/pages/infinityScroll"}>Infinity Scroll</Link>
+      </article>
+    </main>
+  );
 }
